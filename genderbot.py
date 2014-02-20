c6ac02fb0abf9fafcc04add2bc9ce5d1118b9d42
Danielle Sucher <dsucher@gmail.com>
2014-02-20 01:00:46 -0500

    Don't bother tweeting probably-boring phrases

--- genderbot.py
+++ genderbot.py
@@ -5,18 +5,27 @@ import wikipedia
 
 
 class Genderbot(TwitterBot):
+  boring_article_regex = (r"municipality|village|town|football|genus|family|"
+                           "administrative|district|community|region|hamlet|"
+                           "school|actor|mountain|basketball|city|species|film|"
+                           "county|located|politician|professional|settlement")
 
   def tweet(self):
     article = self.__random_wikipedia_article()
     match = re.search(r"\bis [^.?]+", article.content, re.UNICODE)
     if match:
       status = self.__format_status(match.group(0), article.url)
-      self.post_tweet(status)
+      if self.__is_interesting(status):
+        self.post_tweet(status)
 
   def __format_status(self, is_phrase, url):
     status = 'gender %s' % (is_phrase)
     if len(status) > 114: status = status[0:113] + '...'
     return status + ' %s' % (url)
+
+  def __is_interesting(self, status):
+    boring_match = re.search(Genderbot.boring_article_regex, status, re.UNICODE)
+    return boring_match is None
 
   def __random_wikipedia_article(self):
     random_title = wikipedia.random(pages=1)
